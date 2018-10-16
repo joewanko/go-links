c1b4804590a30e59153973aa94efd8fbabe9d30b
Joe Wanko <joe@joewanko.com>
2018-10-15 18:11:40 -0700

    Updated the index file to handle trailing slashes.

--- src/index.php
+++ src/index.php
@@ -1,12 +1,28 @@
-<?php 
+<?php
 $links = parse_ini_file('links.ini');
 
-if(isset($_GET['l']) && array_key_exists($_GET['l'], $links)){
-    header('Location: ' . $links[$_GET['l']]);
+function unknown($to_print) {
+    header('HTTP/1.0 404 Not Found');
+    print_r($to_print);
+    echo '<br>Unknown link.';
+}
+
+if(isset($_GET['l'])) {
+    $pieces_raw = split('/', $_GET['l']);
+    $pieces = [];
+    foreach ($pieces_raw as $piece) {
+        if (strlen($piece) !== 0) {
+            array_push($pieces, $piece);
+        }
+    }
+    if(array_key_exists($pieces[0], $links)){
+        header('Location: ' . $links[$pieces[0]]);
+    } else {
+        unknown($pieces);
+    }
 }
 else{
-    header('HTTP/1.0 404 Not Found');
-    echo 'Unknown link.';
+    unknown($_GET['l']);
 }
 
 ?>
